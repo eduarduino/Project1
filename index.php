fb8597a81bf3938c713c1a62fd7282b5600424ae
Eduardo N Siguenza unknown student <ens3@afsconnect2.njit.edu>
2015-03-30 23:06:57 -0400

    Finally got links to work properly

--- index.php
+++ index.php
@@ -10,32 +10,32 @@
 			public $csv;
 			public $columns;
 			public function readcsv($csv, $columns){
-			ini_set('auto_detect_line_endings', true);
-			if(($open = fopen($csv, "r")) !== false){
-				while (($row = fgetcsv($open, ",")) !== false){
-				if ($columns == true){
+			ini_set('auto_detect_line_endings', TRUE);
+			if(($open = fopen($csv, "r")) !== FALSE){
+				while (($row = fgetcsv($open, ",")) !== FALSE){
+				if ($columns == TRUE){
 					$column = $row;
-					$columns = false;
+					$columns = FALSE;
 				}
 				else{
-					$record = array_combine($column, $row);
-					$vals[] = $val;
+					$field = array_combine($column, $row);
+					$fields[] = $field;
 					$name[] = $row[1];					
 				}
 			}
 			fclose($open);
 			}
 		if(empty($_GET)){
-			foreach($vals as $val){
+			foreach($fields as $field){
 				$i++;
-				$records = $i -1;
-				echo '<center><a href='.'"http://web.njit.edu/~ens3/is218Pro/index.php?record='.$records.'"'.'>'.$name[$records].'</a></center>';
+				$recs = $i -1;
+				echo '<center><a href='.'"https://web.njit.edu/~ens3/is218Pro/index.php?records='.$recs.'"'.'>'.$name[$recs].'</a></center>';
 				echo '</p>';
 				}
 		}
-		$val = $vals[$_GET['records']];
-		echo"<table border='1' align='center'>";
-		foreach($val as $csvName => $actName){
+		$field = $fields[$_GET['records']];
+		echo"<table border='1' cellpadding='2' align='center'>";
+		foreach($field as $csvName => $actName){
 			echo"<tr>";
 			echo"<th>$csvName</th><td>$actName</td>";
 			echo"</tr>";
@@ -44,7 +44,7 @@
 		}
 	}
 	$newfile = new csvFile();
-	$newfile->readcsv("hd2013.csv", true);
+	$newfile->readcsv("hd2013.csv", TRUE);
 	?>
 </body>
 </html>

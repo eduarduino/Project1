a53bd05801e0584b45b522861036ef79f72e8e2b
Eduardo N Siguenza unknown student <ens3@afsconnect2.njit.edu>
2015-03-30 20:27:09 -0400

    Created class to read CSV file

--- index.php
+++ index.php
@@ -4,9 +4,31 @@
 </head>
 
 <body>
-<p align="center"><b>Integrated Post Secondary Educational Data System</b></p>
+<p align="center"><b>Educational Data System</b></p>
 	<?php
-		echo "Hello world!";
+		class csvFile{
+			public $csv;
+			public $columns;
+			public function readcsv($csv, $columns){
+			ini_set('auto_detect_line_endings', true);
+			if(($open = fopen($csv, "r")) !== false){
+				while (($row = fgetcsv($open, ",")) !== false){
+				if ($columns == true){
+					$column = $row;
+					$columns = false;
+				}
+				else{
+					$record = array_combine($column, $row);
+					$name[] = $row[1];
+					$vals[] = $val;
+				}
+			}
+			fclose($open);
+			}
+		}
+		
+	$newfile = new csvFile();
+	$newfile->readcsv("hd2013.csv", true);
 	?>
 </body>
 </html>

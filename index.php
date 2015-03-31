d8dfc0ebe937e5386b16915e8f99c2be245fded9
Eduardo N Siguenza unknown student <ens3@afsconnect2.njit.edu>
2015-03-30 20:02:09 -0400

    Hello world!

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>is218 Project(1)</title>
+</head>
+
+<body>
+<p align="center">Integrated Post Secondary Educational Data System</p>
+	<?php
+		echo "Hello world!";
+	?>
+</body>
+</html>
